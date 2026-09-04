86b6576725f3b60ecd6fa557729b274dbc3d4b39
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-09-03 22:45:21 -0600

    Fix lighting update on queue break finally!

--- src/main/java/com/immersiveconvergence/mixin/MultiblockPartBlockMixin.java
+++ src/main/java/com/immersiveconvergence/mixin/MultiblockPartBlockMixin.java
@@ -4,15 +4,24 @@ import com.immersiveconvergence.api.multiblock.QueueProcessor;
 
 import blusunrize.immersiveengineering.api.multiblocks.blocks.registry.MultiblockPartBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import javax.annotation.Nonnull;
 
 @Mixin(MultiblockPartBlock.class)
-public abstract class MultiblockPartBlockMixin {
+public abstract class MultiblockPartBlockMixin extends Block {
+    private MultiblockPartBlockMixin(Properties properties) { super(properties); }
+
+    @Override public int getLightBlock(@Nonnull BlockState state, @Nonnull BlockGetter level, @Nonnull BlockPos pos) { return 0; }
+
+    @Override public boolean propagatesSkylightDown(@Nonnull BlockState state, @Nonnull BlockGetter level, @Nonnull BlockPos pos) { return true; }
+
     @Inject(method = "onRemove", at = @At("RETURN"))
     private void ic$clearBreakTracking(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving, CallbackInfo ci) {
         QueueProcessor.currentlyBreakingPos = null;
